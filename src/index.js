b6dfd63e1a6d443aec356195a49b18536c8fc400
Da-niel-G <da-niel-g.github.com>
2020-10-03 23:54:57 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+  if (array == 0 || !array) {
+    return 0;
+  }
+  else {return Math.min(...array);}
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+  if (array == 0 || !array) {
+    return 0;
+  }
+  else {return Math.max(...array);}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+  if (array == 0 || !array) {
+    return 0;
+  }
+  else {return array.reduce((a,b) => a + b, 0) / array.length;}
 }
